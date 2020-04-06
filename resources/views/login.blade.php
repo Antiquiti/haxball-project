20cfc038c7cc0d5e76528bca4425ac29d974f29b
vagrant <vagrant@homestead>
2020-04-06 10:39:26 +0000

    wersja pierwsza

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,22 +3,24 @@
         <link rel="stylesheet" href="/css/login.css">
     </head>
     <body> 
-        <section class="center-section">
-            <div class="window">
-                <h2>Zaloguj się</h2>
-                <div class="center-vertical">
-                    <form>
-                        <input type="text" placeholder="Login"/><br>
-                        <input type="text" placeholder="Hasło"/><br>
-                        <input type="radio"/><label>Zapamiętaj mnie</label>
-                    </form>
-                </div>
-                <div>
-                    <a href="/">Zarejestruj się</a>
-                    <button class="redBtn">WYŚLIJ</button>
-                </div>
+        <div class="welcome-window">
+            <h2>Zaloguj się</h2>
+            <div class="welcome-form">
+                <form>
+                    <div><input type="text" placeholder="Login"/></div>
+                    <div><input type="text" placeholder="Hasło"/></div>
+                    <div class="checkbox-container"><div class="custom-checkbox"><input type="checkbox"/><div class="background"></div></div>Zapamiętaj mnie</div>
+                </form>
             </div>
-        </section>
+            <div class="welcome-actions">
+                <div class="fill">
+                    <a href="/">Zarejestruj się</a>
+                </div>
+
+                <button class="primitive-button">WYŚLIJ</button>
+            </div>
+        </div>
+        
         <section class="version">
             V1.0.0
         </section>
