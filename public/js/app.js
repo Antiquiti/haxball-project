b37e961653e12249d70bef5920e87754e3f51980
vagrant <vagrant@homestead>
2020-04-24 14:17:54 +0000

    overlay events and roboto

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function()
+{
+    overlay("#settings-ib", ".settings-background");
+    overlay("#notifications-ib", ".notifications-background");
+});
+
+function overlay(button, element)
+{
+    $(button).click(function()
+    {
+        $(element).css("display", "flex");
+        $(element).fadeTo(250, 1);
+    });
+
+    $(element).click(function(event)
+    {
+        if (event.target != this)
+        {
+            return;
+        }
+
+        $(this).fadeTo(250, 0, function()
+        {
+            $(this).css("display", "none");
+        });
+    });
+}
